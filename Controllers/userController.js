713ee8fc3748e582a24d2086714b20bdea964946
Muhammad Ravat <m0ravat736@gmail.com>
2025-02-22 21:50:46 +0000

    error handling implemented

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,19 +1,46 @@
 const User = require('../Models/user');
+const handleErrors = (err) => {
+  let errors = { email: "", password: "" };
+  if (err.code ==11000){
+    errors.email = "That email is already registered on our database";
+  }
+  console.log(err.message, err.code);
+  console.log("Validation errors:", err.errors);  // Log the full error object to inspect it
+
+
+  if (err.message.includes("User validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      console.log(properties); // Log the properties of each error
+      errors[properties.path] = properties.message; // Update the error message for each field
+    });
+  }
+
+  return errors;
+};
 
 
 const signupGet = (req, res) => {
   res.render('signup', { title: 'Create a new user' });
 }
 
-const signupPost = (req, res) => {
-  res.send("New signup");
+const signupPost = async (req, res) => {
+  const {email, password} = req.body;
+
+  try{
+    const user = await User.create({email, password});
+    res.status(201).json(user);
+  }
+  catch(err){
+    const errors = handleErrors(err);
+    res.status(400).json({errors});
+  }
 }
 
 const loginGet = (req, res) => {
   res.render('login', {title: "Login Page"});
 }
 
-const loginPost = (req, res) => {
+const loginPost =  async (req, res) => {
   res.send("New login")
 }
 
--- Models/user.js
+++ Models/user.js
@@ -1,19 +1,20 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const {isEmail} = require('validator');
 
+const Schema = mongoose.Schema;
 const userSchema = new Schema({
-  username: {
+  email: {
     type: String,
-    required: true,
+    required: [true, "Please enter an email"],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, "Please enter a valid email"]
   },
   password: {
     type: String,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true
-  },
+    required: [true, "Please enter a password"],
+    minlength: [6, "Minimum length is 6 characters"]
+  }
 });
 
 const User = mongoose.model('User', userSchema);
