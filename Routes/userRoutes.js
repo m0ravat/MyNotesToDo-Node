152a9bf3bd1dd7126230f2287f39cfec94368402
Muhammad Ravat <m0ravat736@gmail.com>
2025-02-23 18:31:38 +0000

    logout implemented

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -7,5 +7,5 @@ router.get('/signup', userController.signupGet);
 router.post('/signup', userController.signupPost);
 router.get('/login', userController.loginGet);
 router.post('/login', userController.loginPost);
-
+router.get('/logout', userController.logoutGet);
 module.exports = router;
