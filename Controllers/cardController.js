3c6e5f97a877e30044ce71f3fdd43c381fa10aca
Muhammad Ravat <m0ravat736@gmail.com>
2025-02-27 21:25:25 +0000

    implemented card PUT and DELETE functionality

--- Controllers/cardController.js
+++ Controllers/cardController.js
@@ -24,11 +24,38 @@ const Project = require ('../Models/project');
         res.status(400).json({ err});  // Send back errors if any
     }
 };
-const cardUpdate = (req, res) => {
-
+const cardUpdate = async (req, res) => {
+    const {title, description, priority, done} = req.body;
+    try{
+        const card = await Card.findByIdAndUpdate(
+            req.params.id,
+            { title: title, description: description, priority: priority, done: done },
+            { new: true }
+        )
+        const data = res.json(card);
+        console.log("Card data");
+        console.log(data);
+    } catch (err){
+        res.status(400).json(err);  // Send back errors if any
+    }
 }
-const cardDelete = (req, res) => {
+const cardDelete = async (req, res) => {
+    const { id } = req.params;
 
+    try {
+        // Check if the project exists
+        const card = await Card.findById(id);
+        if (!card) {
+            return res.status(404).json({ message: "Card not found" });
+        }
+
+        // Delete the project
+        await Card.findByIdAndDelete(id);
+        res.status(200).json({ message: "card deleted successfully" });
+    } catch (error) {
+        console.error("Error deleting card:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
 }
 
 module.exports = { 
