0a8016d3da3da89d191ab1fe84d183fa1ba131bb
Muhammad Ravat <m0ravat736@gmail.com>
2025-02-28 17:36:25 +0000

    fixing css links pt2 :(

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ mongoose.connect(dbURI)
   
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(cookieParser());
