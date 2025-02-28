7cda97717f15f16325bf41b96e972f6ae6293960
Muhammad Ravat <m0ravat736@gmail.com>
2025-02-28 18:29:31 +0000

    pt 6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./views/**/*.ejs",  // Ensure EJS files are included
+    "./Views/**/*.ejs",  // Updated to match your folder case (Views instead of views)
     "./public/**/*.css",
+    "./public/**/*.js",  // Added JavaScript files in case you have any JS with Tailwind classes
     "./partials/**/*.ejs"
   ],
   theme: {
