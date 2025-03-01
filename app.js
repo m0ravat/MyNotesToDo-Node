094bd6c7c2f9d0ef60070658482a84601f62b30f
Muhammad Ravat <m0ravat736@gmail.com>
2025-03-01 14:50:55 +0000

    using ejs styling

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'Views'));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
