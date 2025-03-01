11db471558164e0aaad19da1e7349058f6f8f588
Muhammad Ravat <m0ravat736@gmail.com>
2025-03-01 15:05:37 +0000

    Revert "made views/partials lowercase"
    This reverts commit 89070bdc95e91e33e068e6998c90bd48d827f0f7.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const path = require('path');
 
 const app = express();
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'Views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
